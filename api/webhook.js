b467fd3d67804285aa200cbe2c7a5f1a3834a9e0
Daniel Bueno <danibuenofernandez@gmail.com>
2024-09-30 15:41:14 +0200

    t

--- api/webhook.js
+++ api/webhook.js
@@ -4,24 +4,20 @@ const VERIFY_TOKEN = "3762b1f82fe66627a9e96587044c3740bb68255a"; // Coloca tu ve
 
 app.use(express.json());
 
-// Ruta para verificar la suscripción de Strava
 app.get('/webhook', (req, res) => {
     console.log('Query parameters:', req.query);
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
+    console.log(`Received mode: ${mode}, token: ${token}, challenge: ${challenge}`);
+    console.log(`Expected token: ${VERIFY_TOKEN}`);
+
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log('Token matched! Verification successful.');
         res.json({ 'hub.challenge': challenge });
     } else {
+        console.log('Verification failed: Mode or token did not match.');
         res.status(403).send('Verification failed');
     }
 });
-
-// Ruta para manejar las notificaciones de Strava
-app.post('/webhook', (req, res) => {
-    console.log('Received event:', req.body);
-    res.sendStatus(200); // Confirmar recepción de la notificación
-});
-
-module.exports = app;
