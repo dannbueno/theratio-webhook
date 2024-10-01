d94ddb199ebf25a843c3a3a4ea8206ad508126e0
Daniel Bueno <danibuenofernandez@gmail.com>
2024-10-01 16:06:25 +0200

    webhook

--- api/webhook.js
+++ api/webhook.js
@@ -1,31 +1,25 @@
-const express = require('express');
+const VERIFY_TOKEN = "13f962c46ab51d30f8d71485409831ebb6d1469e";
+
+const express = require("express");
 const app = express();
-const VERIFY_TOKEN = "902edd8098d4abf617edb6e2a396a5e947d27bc2";
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.redirect('/webhook');
-});
-
-app.get('/webhook', (req, res) => {
-    console.log('Verificando Webhook...');
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        console.log('Webhook verificado exitosamente');
-        res.json({ 'hub.challenge': challenge });
-    } else {
-        console.log('Verificación fallida');
-        res.status(403).send('Verification failed');
-    }
-});
 
 app.post('/webhook', (req, res) => {
-    console.log('Evento recibido:', req.body);
-    res.sendStatus(200);
+    console.log("webhook event received!", req.query, req.body);
+    res.status(200).send('EVENT_RECEIVED');
 });
 
+app.get("/webhook", (req, res) => {
+    res.send("Express on Vercel!!!");
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    console.log('mode: ' + mode);
+    console.log('token: ' + token);
+    console.log('VERIFY_TOKEN: ' + VERIFY_TOKEN);
+    console.log('challenge: ' + challenge);
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 module.exports = app;
